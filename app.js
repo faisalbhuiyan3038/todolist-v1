9e7b35750535b2f129f327da24a92fc019aba887
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-03-31 20:22:00 +0600

    h1 set as current day

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine","ejs");
 
 app.get("/", function(req, res){
-    res.send("Hello");
+    var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {
+        kindOfDay: day
+    });
 });
 
 app.listen(3000, function(){
