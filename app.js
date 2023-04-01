7f1c9b83a116e5f18bb8a9a273342037d9fdc58b
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-04-01 20:32:05 +0600

    separated header and footer as templates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 let items = ["Buy Food","Cook Food","Eat Food"];
+let workItems = [];
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -18,15 +19,30 @@ app.get("/", function(req, res){
     let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
-        kindOfDay: day,
+        listTitle: day,
         newListItems: items
     });
 });
 
 app.post("/", function(req,res){
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
+    
+});
+
+app.get("/work", function(req,res){
+    res.render("list",{listTitle: "Work List",newListItems: workItems});
+});
+
+app.get("/about", function(req,res){
+    res.render("about");
 });
 
 app.listen(3000, function(){
