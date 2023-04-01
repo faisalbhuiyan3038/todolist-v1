fe457cb361d1a27360daa6d510ab7b3141ed0165
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-04-01 20:03:17 +0600

    updated style

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var items = [];
+let items = ["Buy Food","Cook Food","Eat Food"];
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.set("view engine","ejs");
 
 app.get("/", function(req, res){
-    var today = new Date();
-    var options = {
+    let today = new Date();
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
         kindOfDay: day,
@@ -23,7 +24,7 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req,res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     items.push(item);
     res.redirect("/");
 });
